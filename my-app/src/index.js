06fbfc94963d5c4ec8d4d6848277d0f7e2370173
Rafael Goncalves <rafaelgoncalves@riseup.net>
2020-12-02 09:32:39 -0300

    [TR-15] Muda componentes sem estado para função

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function Square(props) {
-    return (
+const Square =  (props) => (
       <button
         className="square"
         onClick={props.onClick}
@@ -12,41 +11,38 @@ function Square(props) {
         {props.value}
       </button>
     );
-}
 
-class Board extends React.Component {
+const Board =  (props) => {
 
-  renderSquare(i) {
+  function renderSquare(i) {
     return (
       <Square
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
+      value={props.squares[i]}
+      onClick={() => props.onClick(i)}
         />
     );
-  }
+  };
 
-  render() {
-    return (
+   return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
         </div>
       </div>
     );
   }
-}
 
 class Game extends React.Component {
   constructor(props) {

6d23e2ee579b4c4b59df9fcef03f1db74a0aa4bf
Rafael Goncalves <rafaelgoncalves@riseup.net>
2020-12-02 08:42:56 -0300

    [TR-14] Mantem o historico apenas de jogadas passadas

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -61,7 +61,9 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(
+      0, this.state.stepNumber + 1
+    );
     const current = history[history.length - 1];
     const squares = {...current.squares};
     if (calculateWinner(squares) || squares[i]) {
@@ -72,6 +74,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -85,7 +88,7 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const moves = history.map((step, move) => {
       const desc = move ?
